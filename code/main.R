7e29925c76a09c77d099f18059ed34ea0e348ab8
seongjinleeeee <sjlee0702@snu.ac.kr>
2021-11-26 21:28:30 -0500

    Second update
    1. log transformations for predictors
     - Use AIC to select between the original value and the log value
     - pH value is already a log of something, so we do not take the log
     - citric.acid as 0 values so we cannot use the log transformation
     - The method is kinda crude, it just updates the variables in the original order, which can be troublesome.
    
    2. Variable selection
     - We use regsubsets since there are not much predictors.
     - It seems we don't have to drop any variables.
     - using glmnet for LASSO also gives similar results.
    
    3. Model evaluation
     - This is described in detail in the latex file, but I think it's reasonable to look at the contingency table.
    
    4. Resampling?(WLS)
     - The data is imbalanced, most of the data are in 5~6.
     - We use the weight option to give inverse sqrt cnt weight to the samples.
     - Seems more balanced(?)

--- code/main.R
+++ code/main.R
@@ -3,16 +3,17 @@
 library(leaps)
 
 
-### 1. Reading data and dealing with NA values 
+### 0. Data Preprocessing
 red <- read.csv("../data/winequality-red.csv", header = TRUE, sep = ";")
 
-##SJ## Using .. allows the code to work for anyone who clones this; main.R is 
-##SJ## in /code while the data is in /data.
+## Using .. allows the code to work for anyone who clones this; main.R is 
+## in /code while the data is in /data. If the working directory is set
+## as the directory of main.R, this should work.
 
 #white <- read.csv("./data/winequality-white.csv", header = TRUE, sep = ";")
 
 red[!complete.cases(red),]
-##SJ## This part looks unncecessary; there are no missing values =(
+## This part looks unnecessary; there are no missing values =(
 
 # Delete rows with NA values
 #red <- red[complete.cases(red), ]
@@ -21,12 +22,12 @@ red[!complete.cases(red),]
 # Delete duplicated rows
 #red <- red[!duplicated(red), ]
 # red[duplicated(red), ]
-##SJ## Do we really want to delete the duplicated rows...?
+## Do we really want to delete the duplicated rows...?
 
 
 
 
-### 1.5 Overview of Data
+### 1 Overview of Data
 
 library(tibble)
 library(dplyr)
@@ -63,18 +64,66 @@ summary(lm_red)
 par(mfrow = c(2, 2))
 plot(lm_red)
 
+cor(red)
 
-### 2. Variable Selection
-lm_red <- lm(quality ~ ., red)
+### 3 Transformations
+
+colnames(red)
+
+logred = red
+
+logred$logFA = log(red$fixed.acidity)
+logred$logVA = log(red$volatile.acidity)
+logred$logCA = red$citric.acid
+logred$logRS = log(red$residual.sugar)
+logred$logCL = log(red$chlorides)
+logred$logFS = log(red$free.sulfur.dioxide)
+logred$logTS = log(red$total.sulfur.dioxide)
+logred$logDE = log(red$density)
+logred$logPH = red$pH
+logred$logSP = log(red$sulphates)
+logred$logAL = log(red$alcohol)
+
+lm_all = lm(quality ~., red)
+
+lm_log = lm(quality~. , logred[,12:23])
+
+
+df1 = logred[,1:12]
+lm1 = lm(quality~., df1)
+aic1 = AIC(lm1)
+for(i in 1:11){
+        df2 = df1
+        df2[,i] = logred[12+i]
+        colnames(df2)[i] = colnames(logred[12+i])
+        lm2 = lm(quality~., df2)
+        aic2 = AIC(lm2)
+        if(aic2<aic1){
+                df1 = df2
+                lm1 = lm2
+                aic1 = aic2
+                print(paste0('switching ', i))
+        }
+}
+
+head(df1)
+
+summary(lm1)
+
+
+### 4. Variable Selection
+lm_red <- lm(quality ~ ., df1)
 summary(lm_red)
 
 require(leaps)
-b <- regsubsets(quality ~ ., red)
+b <- regsubsets(quality ~ ., df1)
 rs <- summary(b)
 
 par(mfrow = c(1, 3))
           # AIC
 rs$which
+
+# There's a function AIC, we shouldn't use this variable name
 AIC <- nrow(red)*log(rs$rss/nrow(red)) + (2:9)*2
 plot(AIC ~ I(1:8), 
      #main = "AIC",
@@ -93,4 +142,43 @@ plot(2:9, rs$cp,
      xlab = 'Number of Parameters',
      ylab = 'Cp Statistic', pch = 20)
 abline(0,1)
+
 par(mfrow = c(1, 1))
+
+# LASSO
+
+library(glmnet)
+
+cv_lasso = cv.glmnet(as.matrix(df1[,-1]), df1[,12],
+                     type.measure = 'mae')
+plot(cv_lasso)
+
+# No need for penalization
+
+### 5. Estimation of Error
+
+n = nrow(df1)
+df_train = df1[(1:n)%%10!=0,]
+df_test = df1[(1:n)%%10==0,]
+
+lm_train = lm(quality ~., data = df_train)
+test_pred = predict(lm_final, df_test)
+plot(test_pred, df_test$quality)
+
+mean((test_pred-df_test$quality)^2)
+mean((test_pred-df_test$quality)^2) - 1/12
+
+table(round(test_pred), df_test$quality)
+
+lm_final = lm(quality~., data = df1)
+table(round(predict(lm_final, data = df1)), df1$quality)
+
+
+### 6. Resampling?
+
+
+wt = 1/sqrt(table(df1$quality))
+wts = wt[df1$quality-2]
+
+lm_wt = lm(quality~., data = df1, weights = wts)
+table(round(predict(lm_wt, data = df1)), df1$quality)
